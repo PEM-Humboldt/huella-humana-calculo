b92cfbcaa63cf366faafd8499d321b117d629982
anarvaezv <74357934+anarvaezv@users.noreply.github.com>
2025-07-04 15:02:50 -0500

    Create prueba_categorias.R

---
+++ Codigos/prueba_categorias.R
@@ -0,0 +1,16 @@
+breaks <- c(0, 0, 4,10,22, 100)
+breaks = c(0, 15, 40, 60, 100)
+breaks = c(0,1, 15,  35, 100)# tapia armijos 2017. eme gusta
+labels <- c("Natural", "Baja", "Media", "Alta", "Muy Alta")
+
+
+breaks/100*38
+
+library(sf)
+library(dplyr)
+ecos <- st_read("~/fplv/ecos_vereda.shp")
+
+a <- ecos %>% 
+  select(ecos_gener, cob)
+
+unique(a$ecos_gener)
