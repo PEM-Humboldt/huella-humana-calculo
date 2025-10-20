6ed5503134adf40f4e83a2e051d8dbd376a42318
anarvaezv <74357934+anarvaezv@users.noreply.github.com>
2025-10-20 10:00:44 -0500

    Delete prueba_categorias.R

--- Codigos/prueba_categorias.R
+++
@@ -1,8 +0,0 @@
-breaks <- c(0, 0, 4,10,22, 100)
-breaks = c(0, 15, 40, 60, 100)
-breaks = c(0,1, 15,  35, 100)# tapia armijos 2017. eme gusta
-labels <- c("Natural", "Baja", "Media", "Alta", "Muy Alta")
-
-
-breaks/100*38
-
